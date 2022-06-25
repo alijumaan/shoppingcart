1b217cfbae83d8732a4efdd4b914a9f817d6aae0
Ali Al Qahtani <73781540+alijumaan@users.noreply.github.com>
2022-06-26 00:31:39 +0300

    create database and config file

--- src/ShoppingCartServiceProvider.php
+++ src/ShoppingCartServiceProvider.php
@@ -33,7 +33,7 @@ class ShoppingCartServiceProvider extends ServiceProvider
             $timestamp = date('Y_m_d_His', time());
 
             $this->publishes([
-                __DIR__ . '/../database/migrations/0000_00_00_000000_create_shopping_cart_table.php' => database_path('migrations/'.$timestamp.'_create_shopping_cart_table.php'),
+                __DIR__ . '/../database/migrations/0000_00_00_000000_create_shopping_cart_table.php' => database_path('migrations/'.$timestamp.'_create_shoppingcart_table.php'),
             ], 'migrations');
         }
     }
